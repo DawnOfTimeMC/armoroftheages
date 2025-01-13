f2d3caf796607ee6a37ad45fea2d64ae60c8c85f
Calum <hendersoncal117@gmail.com>
2025-01-13 11:47:46 +0000

    Config screen finalized. Need to implement now.

--- common/src/main/java/org/dawnoftime/armoroftheages/config/AOTAConfig.java
+++ common/src/main/java/org/dawnoftime/armoroftheages/config/AOTAConfig.java
@@ -1,10 +1,7 @@
 package org.dawnoftime.armoroftheages.config;
 
 import com.google.gson.GsonBuilder;
-import dev.isxander.yacl3.api.ConfigCategory;
-import dev.isxander.yacl3.api.Option;
-import dev.isxander.yacl3.api.OptionDescription;
-import dev.isxander.yacl3.api.YetAnotherConfigLib;
+import dev.isxander.yacl3.api.*;
 import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
@@ -31,15 +28,25 @@ public class AOTAConfig {
     public PreferredModel preferredModel = PreferredModel.MALE;
 
     public static YetAnotherConfigLib createScreen() {
-        final var notice = Component.translatable("config.armoroftheages.notice");
-
         return YetAnotherConfigLib.create(CONFIG_CLASS_HANDLER,
                 (AOTAConfig defaults, AOTAConfig config, YetAnotherConfigLib.Builder builder) -> {
+                    var preferredModel = Option.<PreferredModel>createBuilder()
+                            .name(Component.translatable("config.armoroftheages.preferred_model"))
+                            .description(OptionDescription.of(Component.translatable("config.armoroftheages.preferred_model.description")))
+                            .binding(defaults.preferredModel, () -> config.preferredModel, val -> config.preferredModel = val)
+                            .controller(opt -> new EnumControllerBuilderImpl<>(opt).enumClass(PreferredModel.class))
+                            .build();
+
                     var usedPreferredModel = Option.<Boolean>createBuilder()
                             .name(Component.translatable("config.armoroftheages.use_preferred_model"))
                             .description(OptionDescription.of(Component.translatable("config.armoroftheages.use_preferred_model.description")))
                             .binding(defaults.usePreferredModel, () -> config.usePreferredModel, val -> config.usePreferredModel = val)
                             .controller(opt -> new BooleanControllerBuilderImpl(opt).coloured(true).trueFalseFormatter())
+                            .addListener((option, event) -> {
+                                if (event == OptionEventListener.Event.STATE_CHANGE) {
+                                    preferredModel.setAvailable(config.usePreferredModel);
+                                }
+                            })
                             .build();
 
                     var ignoredSynchronizedPreferredModel = Option.<Boolean>createBuilder()
@@ -47,13 +54,6 @@ public class AOTAConfig {
                             .description(OptionDescription.of(Component.translatable("config.armoroftheages.ignore_synchronized_preferred_models.description")))
                             .binding(defaults.ignoredSynchronizedPreferredModel, () -> config.ignoredSynchronizedPreferredModel, val -> config.ignoredSynchronizedPreferredModel = val)
                             .controller(opt -> new BooleanControllerBuilderImpl(opt).coloured(true).trueFalseFormatter())
-                            .build();
-
-                    var preferredModel = Option.<PreferredModel>createBuilder()
-                            .name(Component.translatable("config.armoroftheages.preferred_model"))
-                            .description(OptionDescription.of(Component.translatable("config.armoroftheages.preferred_model.description")))
-                            .binding(defaults.preferredModel, () -> config.preferredModel, val -> config.preferredModel = val)
-                            .controller(opt -> new EnumControllerBuilderImpl<>(opt).enumClass(PreferredModel.class))
                             .build();
 
                     return builder.title(Component.translatable("config.armoroftheages.title"))
@@ -64,8 +64,9 @@ public class AOTAConfig {
                             })
                             .category(ConfigCategory.createBuilder()
                                     .name(Component.translatable("config.armoroftheages.title"))
-                                    .option(usedPreferredModel)
+                                    .option(LabelOption.create(Component.translatable("config.armoroftheages.notice")))
                                     .option(ignoredSynchronizedPreferredModel)
+                                    .option(usedPreferredModel)
                                     .option(preferredModel)
                                     .build());
                 });
